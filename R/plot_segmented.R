8e029443c68bf3f3455111bc47ad7c4095d7261a
Alan Inglis <alaninglis@MAC-9DD782.station>
2025-06-30 22:02:17 +0100

    removed arrow

--- R/plot_segmented.R
+++ R/plot_segmented.R
@@ -4,17 +4,17 @@
 #'
 #' @param original_img A \code{magick-image} object representing the original image.
 #' @param segmented_img A \code{magick-image} object representing the segmented image.
-#' @param divider Character. One of \code{"line"}, \code{"arrow"}, or \code{"none"}. Default is \code{"line"}.
+#' @param divider Character. One of \code{"line"} or \code{"none"}. Default is \code{"line"}.
 #' @param show_title Logical. Whether to show image titles above the panels. Default is \code{TRUE}.
 #'
 #' @return A side-by-side visual plot of the two images using the \code{grid} graphics system.
 #'
-#' @importFrom grid grid.newpage pushViewport viewport grid.layout grid.raster grid.text grid.lines grid.arrows popViewport gpar
+#' @importFrom grid grid.newpage pushViewport viewport grid.layout grid.raster grid.text grid.lines popViewport gpar
 #' @importFrom grDevices as.raster
 #' @export
 plot_segmented <- function(original_img, segmented_img, divider = "line", show_title = TRUE) {
   # Validate divider input
-  divider <- match.arg(divider, choices = c("line", "arrow", "none"))
+  divider <- match.arg(divider, choices = c("line",  "none"))
 
   # Convert magick images to raster
   original_raster <- grDevices::as.raster(original_img)
@@ -46,8 +46,5 @@ plot_segmented <- function(original_img, segmented_img, divider = "line", show_t
   # Add divider
   if (divider == "line") {
     grid::grid.lines(x = 0.5, y = c(0, 1), gp = grid::gpar(lwd = 2))
-  } else if (divider == "arrow") {
-    grid::grid.arrows(x = 0.5, y = c(0, 1), gp = grid::gpar(lwd = 2))
-    #grid::grid.arrows(x0 = 0.45, y0 = 0.5, x1 = 0.55, y1 = 0.5, gp = grid::gpar(lwd = 2))
   }
 }
